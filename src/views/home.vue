03b0b7a25cad4af2cbe9116e9ea94668bcea587d
王永森 <wangyongsen@istarshine.com>
2021-04-21 16:27:13 +0800

    #feat 集成 UI 框架 Element Plus

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,4 +1,8 @@
-<template>home.vue</template>
+<template>
+    <h1>home.vue</h1>
+    <router-link to="axios"><el-button>go To axios.vue</el-button></router-link>
+    <router-link to="vuex"><el-button>go To vuex.vue</el-button></router-link>
+</template>
 
 <script>
     export default {
