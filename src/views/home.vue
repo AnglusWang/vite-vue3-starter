7be97c951654a690dd7a4e320661174373cf6c85
王永森 <wangyongsen@istarshine.com>
2021-04-21 16:21:51 +0800

    #feat 集成路由工具 Vue-router

---
+++ src/views/home.vue
@@ -0,0 +1,11 @@
+<template>home.vue</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('🚀 ~ file: home.vue ~ line 8 ~ mounted ~ mounted')
+        }
+    }
+</script>
+
+<style></style>
